159bb6edc61e9f946fce74d2de4b7e214fdb8038
Kamil Głusiński <kamil.glusinski@gmail.com>
2020-10-20 19:28:26 +0200

    added first query

---
+++ src/Application/Query/GetData.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zaprogramowani\Application\Query;
+
+use Zaprogramowani\Application\Exception\AgeOutOfRangeException;
+use Zaprogramowani\Application\Exception\UnknownPlaceTypeException;
+
+class GetData
+{
+    private const TYPE_CITY = 0;
+    private const TYPE_RURAL = 1;
+
+    private $age;
+    private $place;
+    private $placeType;
+
+    public function __construct(int $age, int $place, int $placeType)
+    {
+        if ($age < 0 || $age > 100) {
+            throw new AgeOutOfRangeException();
+        }
+        $this->age = $age;
+
+        $this->place = $place;
+
+        if ($placeType != self::TYPE_CITY && $placeType != self::TYPE_RURAL) {
+            throw new UnknownPlaceTypeException();
+        }
+        $this->placeType = $placeType;
+    }
+
+    public function Age(): int
+    {
+        return $this->age;
+    }
+
+    public function Place(): int
+    {
+        return $this->place;
+    }
+
+    public function PlaceType(): int
+    {
+        return $this->placeType;
+    }
+
+}
