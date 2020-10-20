8d827d7a7d04e3cef72f1bd68f1853ff1866f424
Kamil Głusiński <kamil.glusinski@gmail.com>
2020-10-20 18:57:48 +0200

    added dummy endpoint

---
+++ src/Infra/Controller/DataController.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infra\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DataController
+{
+    public function index(Request $request): Response
+    {
+        return new JsonResponse(
+            [
+                'status' => "ok",
+                'date' => new \DateTime()
+            ],
+            Response::HTTP_OK
+        );
+    }
+}
