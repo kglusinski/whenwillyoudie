236a873e147b6ae65002a9a5299d8878d4cdf27f
Kamil Głusiński <kamil.glusinski@gmail.com>
2020-10-20 20:06:56 +0200

    implemented controller method for fetching data

--- src/Infra/Controller/DataController.php
+++ src/Infra/Controller/DataController.php
@@ -8,9 +8,17 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Zaprogramowani\Application\Query\GetData;
+use Zaprogramowani\Application\Service\MortalityDataServiceInterface;
 
 class DataController
 {
+    private MortalityDataServiceInterface $mortalityDataService;
+
+    public function __construct(MortalityDataServiceInterface $mortalityDataService)
+    {
+        $this->mortalityDataService  = $mortalityDataService;
+    }
+
     public function index(Request $request): Response
     {
         return new JsonResponse(
@@ -30,6 +38,8 @@ class DataController
             $request->get("place_type")
         );
 
+        $view = $this->mortalityDataService->process($query);
 
+        return new JsonResponse($view, Response::HTTP_OK);
     }
 }
