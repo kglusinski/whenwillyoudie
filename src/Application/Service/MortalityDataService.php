7fff8dc7d2c5b07e1be5c66a2985344871da0636
Kamil Głusiński <kamil.glusinski@gmail.com>
2020-10-20 20:21:31 +0200

    created structure for service

--- src/Application/Service/MortalityDataService.php
+++ src/Application/Service/MortalityDataService.php
@@ -4,11 +4,19 @@ declare(strict_types=1);
 
 namespace Zaprogramowani\Application\Service;
 
+use Zaprogramowani\Application\Client\DataSourceClientInterface;
 use Zaprogramowani\Application\Projection\MortalityChanceView;
 use Zaprogramowani\Application\Query\GetData;
 
 class MortalityDataService implements MortalityDataServiceInterface
 {
+    private DataSourceClientInterface $client;
+
+    public function __construct(DataSourceClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
     public function process(GetData $query): MortalityChanceView
     {
 
