e5c22343e2e57cff54bb315f355f94c90e528b65
Kamil Głusiński <kamil.glusinski@gmail.com>
2020-10-22 16:02:07 +0200

    added migrations with initial db state

--- src/Infra/Entity/User.php
+++ src/Infra/Entity/User.php
@@ -4,7 +4,59 @@ declare(strict_types=1);
 
 namespace Zaprogramowani\Infra\Entity;
 
-class User
-{
+use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\ORM\Mapping as ORM;
 
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="users")
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="string")
+     * @ORM\GeneratedValue(strategy="UUID")
+     */
+    private string $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private string $username;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private string $password;
+
+    public function getRoles(): array
+    {
+        return ["user"];
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getPassword(): string
+    {
+        $this->password;
+    }
+
+    public function getSalt()
+    {
+        // Not needed while hashing algorithm is bcrypt
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function eraseCredentials(): void
+    {
+        $this->password = "";
+    }
 }
