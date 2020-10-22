9f75a13dee5f0b62716aa2886476a3e19d4fa70b
Kamil Głusiński <kamil.glusinski@gmail.com>
2020-10-22 10:49:01 +0200

    added empty implementation for data source client

---
+++ src/Application/Client/SQLDataSourceClient.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zaprogramowani\Application\Client;
+
+class SQLDataSourceClient implements DataSourceClientInterface
+{
+
+    public function lifeExpectancy()
+    {
+        // TODO: Implement lifeExpectancy() method.
+    }
+}
